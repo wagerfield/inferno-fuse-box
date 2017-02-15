1f81786f7f53a37f628354e8a562342eae986ffc
Matthew Wagerfield <matthew@wagerfield.com>
2017-02-15 17:01:39 +0000

    Switched to TypeScript

--- fuse.js
+++ fuse.js
@@ -4,8 +4,8 @@ const bundler = new FB.FuseBox({
   homeDir: 'source',
   outFile: 'build/bundle.js',
   plugins: [
-    FB.BabelPlugin()
+    [/\.tsx$/, FB.BabelPlugin()]
   ]
 })
 
-bundler.bundle('>index.jsx')
+bundler.bundle('>index.tsx')

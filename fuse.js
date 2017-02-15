09cf5cfa17719d5eb2e93384601d47be56cf90eb
Matthew Wagerfield <matthew@wagerfield.com>
2017-02-15 20:10:31 +0000

    Refined bundler

--- fuse.js
+++ fuse.js
@@ -1,19 +1,27 @@
 const FB = require('fuse-box')
 
 const fuse = new FB.FuseBox({
-    homeDir: 'source',
-    outFile: 'build/app.js',
-    globals: { inferno: "Inferno" },
-    plugins: [
-        [/\.tsx?$/, FB.BabelPlugin()],
-        //FB.UglifyJSPlugin()
-    ]
+  cache: false,
+  homeDir: 'source',
+  outFile: 'build/app.js',
+  plugins: [
+    [ /\.tsx?$/, FB.BabelPlugin() ],
+    FB.EnvPlugin({
+      NODE_ENV: 'production'
+    }),
+    FB.UglifyJSPlugin()
+  ]
 })
 
-fuse
-    .bundle({
-        "build/vendor.js": `
-          + inferno
-        `
-    })
-    .then(() => fuse.devServer(">[index.tsx]", { port: 3000 }));
+fuse.bundle({
+  'build/vendor.js': '+ inferno',
+  'build/app.js': '> [index.tsx]'
+})
+
+// fuse.bundle({
+//   'build/vendor.js': '+ inferno',
+// }).then(() => {
+//   fuse.devServer('> [index.tsx]', {
+//     port: 3000
+//   })
+// })

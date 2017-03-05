3f74b34e799399205ae9a60da04cdb72f164a9bd
Matthew Wagerfield <matthew@wagerfield.com>
2017-03-05 16:57:22 +0000

    Added generated files to repo

---
+++ build/client.js
@@ -0,0 +1,20 @@
+(function(FuseBox){FuseBox.$fuse$=FuseBox;
+var __process_env__ = {"NODE_ENV":"production"};
+FuseBox.pkg("default", {}, function(___scope___){
+___scope___.file("index.jsx", function(exports, require, module, __filename, __dirname){ 
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var Inferno = require("inferno");
+var createVNode = Inferno.createVNode;
+Inferno.render(createVNode(2, "h1", null, "Inferno \u2764\uFE0F FuseBox"), document.getElementById('root'));
+//# sourceMappingURL=index.jsx.map
+});
+});
+
+FuseBox.import("default/index.jsx");
+FuseBox.main("default/index.jsx");
+})
+(FuseBox)
+//# sourceMappingURL=client.js.map
--- source/index.tsx
+++ source/index.tsx
@@ -1,3 +1,3 @@
 import * as Inferno from 'inferno'
 
-Inferno.render(<h1>Inferno ❤️ FuseBox</h1>, document.getElementById('root'))
+Inferno.render(<h1>Inferno + FuseBox</h1>, document.getElementById('root'))
